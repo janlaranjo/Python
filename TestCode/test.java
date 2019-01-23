2d47f49602d256c1e20e6718254f639d528f63e0
Jan Laranjo <e1425799@student.tuwien.ac.at>
2019-01-23 22:19:01 +0100

    Initial commit

---
+++ TestCode/test.java
@@ -0,0 +1,17 @@
+//test 
+ 
+public class TestClass { 
+	 
+	public void foo(boolean isValid) { 
+		if (isValid) { 
+			System.out.println("valid"); 
+			} else { 
+			 
+		} 
+	} 
+	 
+	@Override 
+	public String toString() { 
+		return "Name"; 
+	} 
+} 
